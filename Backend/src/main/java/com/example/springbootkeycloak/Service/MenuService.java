d083e18ce6388e1e220143dceff7206ce62bc71b
Emmanuel Foeh <EmmanuelFo@stlghana.com>
2024-02-17 09:04:04 +0000

    UI changes

--- Backend/src/main/java/com/example/springbootkeycloak/Service/MenuService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootkeycloak.Service;
-
-import com.example.springbootkeycloak.Model.Menu;
-
-import java.util.Optional;
-
-public interface MenuService {
-    Optional<Menu> findById(Long Id);
-}
